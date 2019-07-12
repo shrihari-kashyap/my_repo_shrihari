37e362ada5ad88faaf30919b687e8dc1c1e207dd
Shrihari R <shrihari.r@qxf2.com>
2019-07-12 10:04:31 +0530

    Least expensive aloe and almond item added to the cart

--- least_expensive_aloe_almond_moisturizers.py
+++ least_expensive_aloe_almond_moisturizers.py
@@ -1,44 +1,24 @@
 import time
+#from selenium import webdriver
 from selenium import webdriver
-
-#create object for webdriver
+#get the chrome driver
 driver=webdriver.Chrome()
+#go to the moisturizer link
 driver.get("https://weathershopper.pythonanywhere.com/moisturizer")
-time.sleep(3)
-
-#check if the heading of the page matches so that we can confirm we have landed on the right page
-if(driver.find_element_by_xpath("//h2").text=="Moisturizers"):
-    print("Successfully entered the Moisturizers shopping site")
-else:
-    print("Failed to to enter the desired page.")
-time.sleep(2)
-#find all the elements on the page which contains 'SPF-50' in it
+#find the xpath for both almond and aloe
 aloe=driver.find_elements_by_xpath("//p[contains(text(),'Aloe')]/following-sibling::p")
-
-#assign an initial minimum price and iterate comparing the prices of all the elements to the minimum price and assign the new minimum price
-min_value_aloe=10000
-for each_element in aloe:
-        #split and extract the prices of item in integer format    
-        price_aloe=int(each_element.text.split()[-1])
-        if(price_aloe<min_value_aloe):
-            min_value_aloe=price_aloe
-
-#find and click the Add button corresponding to the minimum price
-driver.find_element_by_xpath("//p[contains(text(),'%d')]/following-sibling::button"%min_value_aloe).click()      
-print("the sunscreen added to the cart is %s and its price is %d"%(driver.find_element_by_xpath("//p[contains(text(),'%d')]/preceding-sibling::p"%min_value_aloe).text,min_value_aloe))    
-
-#find all the elements on the page which contains 'SPF-30' in it
 almond=driver.find_elements_by_xpath("//p[contains(text(),'Almond')]/following-sibling::p")
-
-#assign an initial minimum price and iterate comparing the prices of all the elements to the minimum price and assign the new minimum price
-min_value_almond=10000
-
-for each_element in almond:
-        #split and extract the prices of item in integer format
-        price_almond=int(each_element.text.split()[-1])
-        if(price_almond<min_value_almond):
-            min_value_almond=price_almond
-
-#find and click the Add button corresponding to the minimum price
-driver.find_element_by_xpath("//p[contains(text(),'%d')]/following-sibling::button"%min_value_almond).click()      
-print("the sunscreen added to the cart is %s and its price is %d"%(driver.find_element_by_xpath("//p[contains(text(),'%d')]/preceding-sibling::p"%min_value_almond).text,min_value_almond))
+least_moisturizer=[almond,aloe]
+#find the least expensive item from aloe and almond
+min_value=10000
+for moist in least_moisturizer:
+    for each_element in moist:    
+        price_moisturizer=int(each_element.text.split()[-1])
+        if(price_moisturizer<min_value):
+            min_value=price_moisturizer
+    #select the item by clicking the add button
+    driver.find_element_by_xpath("//p[contains(text(),'%d')]/following-sibling::button"%min_value).click()
+    time.sleep(2)
+    #print the name of the product and price of aloe and almond
+    print("the moisturizer added to the cart is %s and its price is %d"%(driver.find_element_by_xpath("//p[contains(text(),'%d')]/preceding-sibling::p"%min_value).text,min_value))    
+    time.sleep(3)
