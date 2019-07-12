de816d148dc38e21f2861a146bdf38f49c94715c
Shrihari R <shrihari.r@qxf2.com>
2019-07-12 10:07:55 +0530

    least expensive spf-30 amd spf-50 added to the cart

--- least_expensive_spf_50_spf_30_sunscreens.py
+++ least_expensive_spf_50_spf_30_sunscreens.py
@@ -1,44 +1,27 @@
+"""
+To find the least expensive sunscreen
+"""
 import time
+#from selenium import webdriver
 from selenium import webdriver
-
-#create object for webdriver
+#get the webdriver set to chrome
 driver=webdriver.Chrome()
+#get the website from the driver
 driver.get("https://weathershopper.pythonanywhere.com/sunscreen")
-time.sleep(3)
-
-#check if the heading of the page matches so that we can confirm we have landed on the right page
-if(driver.find_element_by_xpath("//h2").text=="Sunscreens"):
-    print("Successfully entered the sunscreen shopping site")
-else:
-    print("Failed to to enter the desired page.")
-time.sleep(2)
-#find all the elements on the page which contains 'SPF-50' in it
+#check for the condition to select spf-50 and spf-30 items
 spf_50=driver.find_elements_by_xpath("//p[contains(text(),'SPF-50')]/following-sibling::p")
-
-#assign an initial minimum price and iterate comparing the prices of all the elements to the minimum price and assign the new minimum price
-min_value_spf50=10000
-for each_element in spf_50:
-        #split and extract the prices of item in integer format    
-        price_spf50=int(each_element.text.split()[-1])
-        if(price_spf50<min_value_spf50):
-            min_value_spf50=price_spf50
-
-#find and click the Add button corresponding to the minimum price
-driver.find_element_by_xpath("//p[contains(text(),'%d')]/following-sibling::button"%min_value_spf50).click()      
-print("the sunscreen added to the cart is %s and its price is %d"%(driver.find_element_by_xpath("//p[contains(text(),'%d')]/preceding-sibling::p"%min_value_spf50).text,min_value_spf50))    
-
-#find all the elements on the page which contains 'SPF-30' in it
 spf_30=driver.find_elements_by_xpath("//p[contains(text(),'SPF-30')]/following-sibling::p")
-
-#assign an initial minimum price and iterate comparing the prices of all the elements to the minimum price and assign the new minimum price
-min_value_spf30=10000
-
-for each_element in spf_30:
-        #split and extract the prices of item in integer format
-        price_spf30=int(each_element.text.split()[-1])
-        if(price_spf30<min_value_spf30):
-            min_value_spf30=price_spf30
-
-#find and click the Add button corresponding to the minimum price
-driver.find_element_by_xpath("//p[contains(text(),'%d')]/following-sibling::button"%min_value_spf30).click()      
-print("the sunscreen added to the cart is %s and its price is %d"%(driver.find_element_by_xpath("//p[contains(text(),'%d')]/preceding-sibling::p"%min_value_spf30).text,min_value_spf30))
+least_sunscreen=[spf_50,spf_30]
+#find the least expensive item from spf-50 and spf-30
+min_value=10000
+for sun in least_sunscreen:
+    for each_element in sun:    
+        price_sunscreen=int(each_element.text.split()[-1])
+        if(price_sunscreen<min_value):
+            min_value=price_sunscreen
+    #select the item by clicking the add button
+    driver.find_element_by_xpath("//p[contains(text(),'%d')]/following-sibling::button"%min_value).click()
+    time.sleep(2)
+    #Print the least expensive spf-50 and spf-30 items
+    print("the sunscreen added to the cart is %s and its price is %d"%(driver.find_element_by_xpath("//p[contains(text(),'%d')]/preceding-sibling::p"%min_value).text,min_value))    
+    time.sleep(3)
